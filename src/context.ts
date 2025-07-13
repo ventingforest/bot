8da17d4f32664e621f0ce4a36efba715a37914c3
newt <hi@newty.dev>
2025-07-13 23:20:31 +0100

    feat: split logging

--- src/context.ts
+++ src/context.ts
@@ -1,9 +1,43 @@
+import { configure, getConsoleSink, getLogger } from "@logtape/logtape";
 import { PrismaClient } from "./generated/prisma";
-import { getLogger } from "@logtape/logtape";
-import { Client } from "discord.js";
+import { prettyFormatter } from "@logtape/pretty";
+import { Client, Events } from "discord.js";
 
+// --- logtape ---
+await configure({
+  sinks: {
+    console: getConsoleSink({
+      formatter: prettyFormatter,
+    }),
+  },
+  loggers: [
+    { category: "bot", lowestLevel: "debug", sinks: ["console"] },
+    { category: "discord", lowestLevel: "debug", sinks: ["console"] },
+    { category: "prisma", lowestLevel: "warning", sinks: ["console"] },
+    {
+      category: ["logtape", "meta"],
+      lowestLevel: "warning",
+      sinks: ["console"],
+    },
+  ],
+});
+
+export const logger = getLogger(["bot"]);
+
+// --- prisma ---
+export const prisma = new PrismaClient({
+  log: ["info", "warn", "error"],
+});
+const prismaLogger = getLogger(["prisma"]);
+prisma.$on("info" as never, e => prismaLogger.info(e));
+prisma.$on("warn" as never, e => prismaLogger.warn(e));
+prisma.$on("error" as never, e => prismaLogger.error(e));
+
+// --- discord ---
 export const client = new Client({
   intents: [],
 });
-export const prisma = new PrismaClient();
-export const logger = getLogger(["bot"]);
+const discordLogger = getLogger(["discord"]);
+client.on(Events.Debug, message => discordLogger.debug(message));
+client.on(Events.Warn, message => discordLogger.warn(message));
+client.on(Events.Error, ({ message }) => discordLogger.error(message));
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,8 @@
-import { configure, getConsoleSink } from "@logtape/logtape";
-import { prettyFormatter } from "@logtape/pretty";
 import { client, logger } from "$ctx";
 import { Events } from "discord.js";
-
-// configure logtape
-await configure({
-  sinks: {
-    console: getConsoleSink({
-      formatter: prettyFormatter,
-    }),
-  },
-  loggers: [
-    { category: "bot", lowestLevel: "debug", sinks: ["console"] },
-    {
-      category: ["logtape", "meta"],
-      lowestLevel: "warning",
-      sinks: ["console"],
-    },
-  ],
-});
 
 client.once(Events.ClientReady, () => {
   logger.info`Logged in as ${client.user?.tag}`;
 });
-
-client.on(Events.Debug, message => logger.debug(message));
-client.on(Events.Warn, message => logger.warn(message));
-client.on(Events.Error, ({ message }) => logger.error(message));
 
 await client.login(process.env.BOT_TOKEN!);
