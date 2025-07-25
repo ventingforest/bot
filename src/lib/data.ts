dde24549a76bd0402ae1432f5ab4abcd85baed63
newt <hi@newty.dev>
2025-07-25 01:56:20 +0100

    feat(anon): send message

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -8,6 +8,17 @@ export const isProduction = process.env.NODE_ENV === "production";
  */
 export const guildId = "435894444101861408";
 
+export const anon = {
+	/**
+	 * The channel ID for the anonymous vent channel.
+	 */
+	channelId: isProduction ? "" : "1397637713980817520",
+	/**
+	 * How long a session lasts before it is cleaned up.
+	 */
+	sessionTimeout: 1000 * 60 * 5, // 5 minutes
+};
+
 export const xp = {
 	/**
 	 * The number of characters in a message required to receive 1 XP.
--- src/listeners/anon/create.ts
+++ src/listeners/anon/create.ts
@@ -1,19 +1,105 @@
-import { type Message, type OmitPartialGroupDMChannel } from "discord.js";
+import { container } from "@sapphire/framework";
+import {
+	type Collection,
+	type Message,
+	type OmitPartialGroupDMChannel,
+	type TextChannel,
+	type Webhook,
+	type WebhookType,
+} from "discord.js";
+import { animals, colors, uniqueNamesGenerator } from "unique-names-generator";
 
-import { channelId } from "$lib/anon";
+import { anon } from "$lib/data";
 import { config, Events, Listener, load } from "$listener";
+
+// user id -> session
+type Session = { webhookId: string; lastActive: number; name: string };
+const sessions = new Map<string, Session>();
+
+const messageQueue: Array<OmitPartialGroupDMChannel<Message>> = [];
 
 @config(Events.MessageCreate)
 class Create extends Listener<typeof Events.MessageCreate> {
 	override async run(message: OmitPartialGroupDMChannel<Message>) {
 		if (message.author.bot || message.webhookId) return; // only listen to users
-		if (message.channelId !== channelId) return; // only handle messages in the anonymous vent channel
+		if (message.channelId !== anon.channelId) return; // only handle messages in the anonymous vent channel
 		await message.delete(); // delete the message to keep it anonymous
-
-		// todo: send the vent
-
-		// await this.container.db.anonVent.create({ data: { message: newMessage.id, user: message.author.id } });
+		await processMessage(message, await fetchWebhooks());
 	}
 }
 
 await load(Create);
+
+async function fetchWebhooks() {
+	const channel = (await container.client.channels.fetch(
+		anon.channelId,
+	)) as TextChannel;
+	return channel.fetchWebhooks();
+}
+
+async function processMessage(
+	message: OmitPartialGroupDMChannel<Message>,
+	webhooks: Collection<
+		string,
+		Webhook<WebhookType.Incoming | WebhookType.ChannelFollower>
+	>,
+	retry = false,
+) {
+	// find the session for this user or create a new one
+	let session = sessions.get(message.author.id);
+	const now = Date.now();
+
+	if (!session || now - session.lastActive > anon.sessionTimeout) {
+		const assigned = new Set([...sessions.values()].map(s => s.webhookId));
+
+		const webhookId = [...webhooks.keys()].find(id => !assigned.has(id));
+		if (!webhookId) {
+			if (!retry) messageQueue.push(message); // if no webhook available, queue the message
+			return;
+		}
+
+		const name = uniqueNamesGenerator({
+			dictionaries: [colors, animals],
+		})
+			.split("_")
+			.map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+			.join("");
+
+		session = { lastActive: now, name, webhookId };
+	} else {
+		session.lastActive = now;
+	}
+
+	sessions.set(message.author.id, session);
+
+	// send the message using the webhook
+	const webhook = webhooks.get(session.webhookId)!;
+	const newMessage = await webhook.send({
+		content: message.content,
+		username: session.name,
+	});
+
+	// store the identity of the user who sent the message
+	await container.db.anonVent.create({
+		data: { message: newMessage.id, user: message.author.id },
+	});
+}
+
+setInterval(async () => {
+	// periodic cleanup
+	const now = Date.now();
+	for (const [userId, { lastActive }] of sessions.entries()) {
+		if (now - lastActive > anon.sessionTimeout) {
+			sessions.delete(userId);
+		}
+	}
+
+	// process queued messages
+	const webhooks = await fetchWebhooks();
+	await Promise.all(
+		messageQueue.map(async message => {
+			await processMessage(message, webhooks, true);
+			messageQueue.splice(messageQueue.indexOf(message), 1);
+		}),
+	);
+}, 1000 * 60); // every minute
